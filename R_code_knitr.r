03a92c915abddb36caad9e55207a5f737699470f
Ludovico Chieffallo <79975866+Ludovico-Chieffallo@users.noreply.github.com>
2021-04-28 01:24:32 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -6,3 +6,16 @@ setwd("~/lab/") # Linux
 # setwd("/Users/name/Desktop/lab/") # Mac 
 
 library(knitr)
+
+#usiamo un gestore di testo dove copieremo tutto il codice che vogliamo importare
+#mettiamo all'interno della nostra cartlla tutto il necessario (sia i file come immagini che il codice)
+
+setwd("c:/lab/greenland1") #questo nel nostro caso ma bisogna selezionare la cartella corrispondente
+getwd() #per controllare la cartella di lavoro
+
+#cosa stiamo facendo in questa maniera? stiamo importando dalla cartella selezionata il file contenente il codice per creare un pdf unico
+#questo pdf avrà al suo interno tutto quello su cui abbiamo lavorato.
+#Per fare questo utilizziamo la funzione stitch dove per prima cosa metteremo il nome del file di testo che abbiamo creato
+stitch("greenland.r", template = system.file("misc", "knitr-template.Rnw", package="knitr"))
+
+#ci restituirà un file leggibile in LaTex con tutto il codice corrispondente
